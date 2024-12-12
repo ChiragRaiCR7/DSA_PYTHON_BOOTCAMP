79d9642965692e65ecbf658fde04fdd8f61d9cdd
ChiragRaiCR7 <chiragraicr7@gmail.com>
2024-12-13 01:12:55 +0530

    Basics of Python

---
+++ commands.py
@@ -0,0 +1,9 @@
+#conda create -p venv python==3.12
+#pip install -r requirements.txt
+#conda activate D:\Python_course\Master_Python_and_Data_Structures\DSA_PYTHON_BOOTCAMP\venv  
+#Conda deactivate 
+#git clone <repository-url>
+# cd <repository-name>
+# git add .
+# git commit -m "Initial commit with project setup"
+# git push origin main
